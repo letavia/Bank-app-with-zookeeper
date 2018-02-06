b4ad4923dd7f7ab36f4638180df0511bedea8b41
letavia <letavia@users.noreply.github.com>
2018-02-06 19:55:40 +0100

    Removed unused methods

--- bankclient/src/zkSendMessages.java
+++ bankclient/src/zkSendMessages.java
@@ -4,11 +4,7 @@ public abstract interface zkSendMessages
 {
   public abstract void sendAdd(ZooKeeper zk, zkClient paramClient);
 
-  //public abstract void sendRead(ZooKeeper zk, Integer paramInteger); handled by local zk server
-
   public abstract void sendUpdate(ZooKeeper zk, zkClient paramClient);
 
   public abstract void sendDelete(ZooKeeper zk, Integer paramInteger);
-
-  //public abstract void sendCreateBank(ZooKeeper zk, zkClientDB paramClientDB); handled by local zk server
 }
